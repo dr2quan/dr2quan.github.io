8125a53dd7300eb729cfa3c9c62d254de13d3e78
dr2quan <131404966+dr2quan@users.noreply.github.com>
2026-02-11 16:16:51 -0500

    Add files via upload

---
+++ src/.config/user.ts
@@ -0,0 +1,27 @@
+import type { UserConfig } from '~/types'
+
+export const userConfig: Partial<UserConfig> = {
+  // Override the default config here
+  site: { title: "表世界郊游",
+          subtitle: "Surface",
+          author: "o-o",
+          description: 'A brief moment on the surface',
+          website: 'https://dr2quan.github.io',
+          socialLinks:[
+            {
+              name: 'rss',
+              href: '/atom.xml',
+            },
+            {
+              name: 'email',
+              href: 'mailto:yu_xue@me.com'
+            }
+
+          ],
+          categoryMap: [{name :'表世界', path:'surface'}],
+  },
+  appearance: {
+    theme: 'light',
+  }
+  // seo: { twitter: "@moeyua13" },
+}
